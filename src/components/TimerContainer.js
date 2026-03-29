89aa5a3f201278846bc95164618ae736cb612c32
Daria Seńko <twoj@email>
2026-03-29 13:10:51 +0200

    01

---
+++ src/components/TimerContainer.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Timer from './Timer';
+
+class TimerContainer extends React.Component {
+    state = {
+        time: 0,
+    }
+
+    getHours() {
+        return Math.floor(this.state.time / 3600);
+    }
+
+    getMinutes() {
+        return Math.floor((this.state.time % 3600) / 60);
+    }
+
+    getSeconds() {
+        return this.state.time % 60;
+    }
+
+    componentDidMount() {
+        this.id = setInterval(() => {
+            this.setState(state => ({
+                time: state.time + 1
+            }));
+        }, 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.id);
+    }
+
+    render() {
+        return (
+            <Timer
+                hours={this.getHours()}
+                minutes={this.getMinutes()}
+                seconds={this.getSeconds()}
+            />
+        );
+    }
+}
+
+export default TimerContainer;
